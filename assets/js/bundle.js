c930e5d603bb18e655bcaf1591d8e73db054e909
Knut Haugen <knut.haugen@nrk.no>
2024-09-13 14:00:52 +0200

    more betterer

---
+++ assets/js/bundle.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  load();
+});
+
+function load() {
+  document.querySelectorAll("summary").forEach((element) => {
+    element.addEventListener("click", (event) => {
+      const detailsElement = event.target.parentElement.parentElement;
+      const contentElement = detailsElement.querySelector("ul");
+      if (contentElement.classList.contains("animation")) {
+        contentElement.classList.remove("animation", "collapsing");
+        void element.offsetWidth;
+        return;
+      }
+
+      const onAnimationEnd = (cb) =>
+        contentElement.addEventListener("animationend", cb, { once: true });
+
+      // request an animation frame to force Safari 16 to actually perform the animation
+      requestAnimationFrame(() => contentElement.classList.add("animation"));
+      onAnimationEnd(() => contentElement.classList.remove("animation"));
+
+      const isDetailsOpen = detailsElement.getAttribute("open") !== null;
+      if (isDetailsOpen) {
+        // prevent default collapsing and delay it until the animation has completed
+        event.preventDefault();
+        contentElement.classList.add("collapsing");
+        onAnimationEnd(() => {
+          detailsElement.removeAttribute("open");
+          contentElement.classList.remove("collapsing");
+        });
+      }
+    });
+  });
+}
--- index.md
+++ index.md
@@ -5,7 +5,7 @@ title: All About the code
 
 <div class="hero display-grid">
 <div class="posts">
-  <div style="grid-area: posts">
+  <div>
   {% for post in site.posts limit:6 %}
      <article>
      <h2><a href="{{ post.url }}">{{ post.title }}</a></h2>
@@ -14,8 +14,4 @@ title: All About the code
     </article>
   {% endfor %}
   </div>
-  <div id="category-sidebar" style="grid-area: cats"><h3>Categories</h3>
-  {% for cat in site.categories %}
-  <a class="cat-button" href="/{{cat[0]}}/">{{cat[0]}}</a>
-  {% endfor %}</div>
 </div>
