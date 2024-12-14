ef854ece575150c861d9955e7a3882d6b2eee2e1
Knut Haugen <knut.haugen@nrk.no>
2024-12-14 09:58:12 +0100

    mobile menu dark mode

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -35,27 +35,32 @@ function darkMode() {
     });
 
   const icon = document.querySelector("#dark-mode");
+  const mobileIcon = document.querySelector("#dark-mode-mobile");
 
-  icon.addEventListener("click", (event) => {
-    const currentMode = localStorage.getItem(modeLocalStorageKey);
-    if (currentMode === "dark") {
-      if (!document.startViewTransition) {
-        setMode("light");
-      } else {
-        document.startViewTransition(() => setMode("light"));
-      }
+  icon.addEventListener("click", darkModeClickHandler);
+  mobileIcon.addEventListener("click", darkModeClickHandler);
+}
+
+function darkModeClickHandler(_event) {
+  const currentMode = localStorage.getItem(modeLocalStorageKey);
+  if (currentMode === "dark") {
+    if (!document.startViewTransition) {
+      setMode("light");
     } else {
-      if (!document.startViewTransition) {
-        setMode("dark");
-      } else {
-        document.startViewTransition(() => setMode("dark"));
-      }
+      document.startViewTransition(() => setMode("light"));
     }
-  });
+  } else {
+    if (!document.startViewTransition) {
+      setMode("dark");
+    } else {
+      document.startViewTransition(() => setMode("dark"));
+    }
+  }
 }
 
 function setMode(mode) {
   const icon = document.querySelector("#dark-mode");
+  const mobileIcon = document.querySelector("#dark-mode-mobile");
 
   if (mode === "dark") {
     localStorage.setItem(modeLocalStorageKey, mode);
@@ -85,7 +90,31 @@ function setMode(mode) {
   <path d="M20 12h2" />
   <path d="m6.3 17.7-1.4 1.4" />
   <path d="m19.1 4.9-1.4 1.4" />
-</svg`;
+</svg>`;
+    mobileIcon.ariaLabel = "Switch to light mode";
+    mobileIcon.title = "Switch to light mode";
+    mobileIcon.innerHTML = `<svg
+  class="lucide lucide-sun-moon icon"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M12 8a2.83 2.83 0 0 0 4 4 4 4 0 1 1-4-4" />
+  <path d="M12 2v2" />
+  <path d="M12 20v2" />
+  <path d="m4.9 4.9 1.4 1.4" />
+  <path d="m17.7 17.7 1.4 1.4" />
+  <path d="M2 12h2" />
+  <path d="M20 12h2" />
+  <path d="m6.3 17.7-1.4 1.4" />
+  <path d="m19.1 4.9-1.4 1.4" />
+</svg> Light Mode`;
   } else {
     localStorage.setItem(modeLocalStorageKey, mode);
     document.querySelector("html").classList.remove("dark");
@@ -107,6 +136,22 @@ function setMode(mode) {
 >
   <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
 </svg>`;
+    mobileIcon.ariaLabel = "Switch to dark mode";
+    mobileIcon.title = "Switch to dark mode";
+    mobileIcon.innerHTML = `<svg
+  class="lucide lucide-moon icon"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+</svg> Dark mode`;
   }
 }
 
