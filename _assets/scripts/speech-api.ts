a9ab18d5a5791e3d0355408d1492d5d8076729cf
Knut Haugen <knut.haugen@nrk.no>
2025-12-26 22:17:30 +0100

    updates

--- _assets/scripts/speech-api.ts
+++ _assets/scripts/speech-api.ts
@@ -1,5 +1,38 @@
 import { findLanguages, hamburgerMenu, addScrollHandler } from "./common.ts";
 
+let langSelector: HTMLSelectElement | null;
+const lang: Record<
+  string,
+  { text: string; author: string; book: string; lang: string }
+> = {
+  english: {
+    text: "We can never be gods, after all — but we can become something less than human with frightening ease.",
+    author: "N.K. Jemisin",
+    book: "The City We Became",
+    lang: "en-GB",
+  },
+  german: {
+    text: "Ein leichtes Leben, eine leichte Liebe, ein leichter Tod – das war nichts für mich.",
+    author: "Herman Hesse",
+    book: "Steppenwolf",
+    lang: "de-DE",
+  },
+  norwegian: {
+    text: "Almennheten behøver slett ingen nye tanker. Almennheten er best tjent med de gamle, gode, og anerkjente tanker den allerede har.",
+    author: "Henrik Ibsen",
+    book: "En folkefiende",
+    lang: "nb-NO",
+  },
+  french: {
+    text: "Dans la vie on ne fait pas ce que l'on veut mais on est responsable de ce que l'on est.",
+    author: "Jean-Paul Sartre",
+    book: "Unknown",
+    lang: "fr-FR",
+  },
+};
+
+let voices: SpeechSynthesisVoice[] = [];
+
 document.addEventListener("DOMContentLoaded", () => {
   load();
   findLanguages();
@@ -7,4 +40,70 @@ document.addEventListener("DOMContentLoaded", () => {
   addScrollHandler();
 });
 
-function load() {}
+function load() {
+  langSelector = document.getElementById(
+    "lang-selector",
+  ) as HTMLSelectElement | null;
+  clickHandlers();
+  populateVoiceList();
+
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+}
+
+function clickHandlers() {
+  langSelector?.addEventListener("change", languageSwitcher);
+}
+
+function languageSwitcher(event: Event) {
+  const selectedLang = (event.target as HTMLSelectElement).value;
+  document.getElementById("lang-source")!.textContent = lang[selectedLang].text;
+  document.getElementById(
+    "citation",
+  )!.textContent = `${lang[selectedLang].author}, ${lang[selectedLang].book}`;
+
+  speak(lang[selectedLang].text, lang[selectedLang].lang);
+}
+
+function speak(text: string, lang: string) {
+  const voiceSelect = document.getElementById(
+    "voice-selector",
+  ) as HTMLSelectElement;
+
+  const utterThis = new SpeechSynthesisUtterance(text);
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name") ?? "unknown";
+
+  for (const voice of voices) {
+    if (voice.name === selectedOption) {
+      utterThis.voice = voice;
+    }
+  }
+
+  utterThis.pitch = 1.0;
+  utterThis.rate = 1.0;
+  speechSynthesis.speak(utterThis);
+}
+
+function populateVoiceList() {
+  const voiceSelect = document.getElementById(
+    "voice-selector",
+  ) as HTMLSelectElement;
+  voices = speechSynthesis.getVoices();
+  for (const voice of voices) {
+    const option = document.createElement("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+
+    if (voice.default) {
+      option.selected = true;
+    }
+
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+  }
+}
--- labs/_posts/2025-12-26-speech-api.md
+++ labs/_posts/2025-12-26-speech-api.md
@@ -11,8 +11,31 @@ category: labs
 
 ## API Overview
 
-Full documentation is available on [MDN web Speech API page](https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API). There are two central interfaces, namely `SpeechRecognition` responsible for recognising spoken text (captured typically via a microfone) and creting text and `SpeechSyntesis` which generates spoken words from text. Of these `SpeechSyntesis` is widely available (baseline) while `SpeechRecognition` is chrome/edge only (some partial support elsewhere, but it's patchy at best) and sends data to a speech recognition service. 
+Full documentation is available on [MDN web Speech API page](https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API). There are two central interfaces, namely `SpeechRecognition` responsible for recognising spoken text (captured typically via a microfone) and creting text and `SpeechSynthesis` which generates spoken words from text. Of these `SpeechSynthesis` is widely available (baseline) while `SpeechRecognition` is chrome/edge only (some partial support elsewhere, but it's patchy at best) and sends data to a speech recognition service. 
 
+Results may wary depending of the available voices and languages on your computer
+
+### Speech Synthesis
+<p>
+<form>
+  <label for="lang-selector">Choose text sample</label>
+  <select id="lang-selector">
+    <option default value="norwegian">Norsk bokmål</option>
+    <option value="english">English</option>
+    <option value="french">Français</option>
+    <option value="german">Deutsch</option>
+  </select>
+</form>
+</p>
+<p class="quote" id="lang-source">"Almennheten behøver slett ingen nye tanker. Almennheten er best tjent med de gamle, gode, og anerkjente tanker den allerede har."</p>
+<p class="citation" id="citation">Henrik Ibsen, En folkefiende</p>
+
+### Speech Controls
+
+<form>
+ <label for="voice-selector">Voice</label>
+ <select id="voice-selector"></select>
+</form>
 
 ### The Code Running on This Page
 
